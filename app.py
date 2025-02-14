ddf84af1d20468abe62da66864d8225e9781203e
Anantha Krishnan <143057072+n-ananthakrishnan@users.noreply.github.com>
2025-02-14 21:22:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -63,4 +63,5 @@ def add_book():
     return jsonify({'message': 'Book added successfully'})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+     app.run(host="0.0.0.0", port=5000, debug=True)
+    
